9d8830e6607f544e14c4187aabbf734a7c67275d
R. Firman Insan M <firman.insan.m@gmail.com>
2025-02-09 01:34:04 +0000

    Initial commit

---
+++ main.scala
@@ -0,0 +1,16 @@
+/* Welcome to the Scala Playground!
+ *
+ * This repl utilizes the `sbtn` thin client.
+ *
+ * To increase compilation speed, it's recommended to
+ * start `sbt` in a Shell tab before clicking "Run".
+ *
+ * This will use a single instance of sbt for all
+ * compilation, instead of launching sbt every time.
+ */
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+  }
+}
